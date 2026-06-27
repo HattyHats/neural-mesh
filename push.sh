26c7e534cf52d5fcdb13c5d34a554183ccc5e950
HattyHats <tonydandria1985@gmail.com>
2026-06-27 08:56:36 -0400

    Add files via upload

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/Desktop/Antigravity/Neural-Mesh
 git add .
-git commit -m "Update UI, remove 3D mode, constellation templates"
+git commit -m "Update UI, remove 3D mode, fix physics bug"
 git push origin main
-git tag v1.1.9
-git push origin v1.1.9
+git tag v1.1.10
+git push origin v1.1.10
